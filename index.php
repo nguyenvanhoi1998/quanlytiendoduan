262fcdb56d9b530503b71ad996d1cc268d792104
nguyenvanhoi1998 <86147388+nguyenvanhoi1998@users.noreply.github.com>
2021-07-23 15:26:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 </head>
 <body style="background-image: url('background.jpg');">
-<div class="container"><h1 style="color:green;text-align:center;">Danh sách các dự án</h1><br>
-	<div>
+<div class="container"><h1 style="color:green;text-align:center;">Danh sách các dự án</h1>
+<a href="themduan.php"><input class="btn btn-outline-primary" type="button" value="Thêm dự án mới"></a><br>
+	<br><div>
 		<table class="table table-bordered" style="text-align:center;">
 			<thead class="thead-dark"><tr><th>Mã dự án</th><th>Tên dự án</th><th>Ngày bắt đầu</th><th>Ngày kết thúc</th><th>Tiến độ</th><th>Dự án con</th><th>Xem tiến độ</th><th>Cập nhật tiến độ</th><th>File đính kèm</th><th colspan=2>Lựa chọn</th></tr></thead>
 			<?php $con = new mysqli("localhost","root","","quanlytiendosinhvienthuctap");
@@ -34,8 +35,7 @@
 				<td><a href="danhsachfiledinhkem.php?maduan=<?php echo $maduan; ?>">Danh sách file</a></td>
 				<td><a href="suaduan.php?maduan=<?php echo $maduan; ?>">Sửa </a></td>
 				<td><a href="xoaduan.php?maduan=<?php echo $maduan; ?>"> Xoá </a></td></tr><?php } ?></tbody>
-		</table><br>
-		<a href="themduan.php"><input class="btn btn-outline-primary" type="button" value="Thêm dự án mới"></a>
+		</table>
 	</div>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
